1da5c3e286df78646fc600f4f71b9e8f5142a1dd
dan <ryu.dan.stu@gmail.com>
2024-10-16 01:48:20 +0900

    タイトル画面作成

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,42 @@
-// スタートボタンの要素を取得
-const startButton = document.getElementById('start-button');
+// 全手動モードボタンの要素を取得
+const allManualButton = document.getElementById('manual-button');
 
-// スタートボタンのクリックイベントリスナー
-startButton.addEventListener('click', () => {
+// 全手動モードボタンのクリックイベントリスナー
+allManualButton.addEventListener('click', () => {
     // game.html に遷移
     window.location.href = 'game.html';
 });
+
+// 読み込み時のイベントリスナー
+document.addEventListener('DOMContentLoaded', () => {
+    // 初期化時に実行
+    resizeAppContainer();
+
+    // 画面リサイズ時に実行
+    window.addEventListener('resize', resizeAppContainer);
+});
+
+/**
+ * 画面比率を調整する
+ */
+function resizeAppContainer() {
+    const appContainer = document.getElementById('app-container');
+    const windowWidth = window.innerWidth;
+    const windowHeight = window.innerHeight;
+    const targetWidth = windowHeight * (16 / 9); // 16:9の比率を維持するための幅
+    const targetHeight = windowWidth * (9 / 16); // 16:9の比率を維持するための高さ
+
+    if (windowWidth / windowHeight >= (16 / 9)) {
+        // 画面比率が16:9より広い場合
+        appContainer.style.width = targetWidth + 'px';
+        appContainer.style.height = windowHeight + 'px';
+        appContainer.style.left = (windowWidth - targetWidth) / 2 + 'px'; // 中央寄せ
+        appContainer.style.top = '0px';
+    } else {
+        // 画面比率が16:9より狭い場合
+        appContainer.style.width = windowWidth + 'px';
+        appContainer.style.height = targetHeight + 'px';
+        appContainer.style.left = '0px';
+        appContainer.style.top = (windowHeight - targetHeight) / 2 + 'px'; // 中央寄せ
+    }
+}
